1d1ab317978f11d0af40602fe420a33633f9cc3b
panicdragon <d.takayama.web@gmail.com>
2022-06-05 16:42:52 +0900

    feat: 🎸 first commit

---
+++ data/soft_list.js
@@ -0,0 +1,657 @@
+module.exports = [
+    {
+        "id": 1,
+        "release_date": "1998-10-28",
+        "title": "キング・オブ・ファイターズR-1",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 4500
+    },
+    {
+        "id": 2,
+        "release_date": "1998-10-28",
+        "title": "ネオジオカップ'98",
+        "publisher": "SNK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 3,
+        "release_date": "1998-10-28",
+        "title": "ポケットテニス",
+        "publisher": "夢工房",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 4,
+        "release_date": "1998-10-28",
+        "title": "めろんちゃんの成長日記",
+        "publisher": "ADK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 5,
+        "release_date": "1998-10-28",
+        "title": "連結パズル つなげてポンッ！",
+        "publisher": "夢工房",
+        "bit_memory": "4M",
+        "price": 3500
+    },
+    {
+        "id": 6,
+        "release_date": "1998-11-20",
+        "title": "将棋の達人",
+        "publisher": "ADK",
+        "bit_memory": "4M",
+        "price": 3500
+    },
+    {
+        "id": 7,
+        "release_date": "1998-11-26",
+        "title": "ベースボールスターズ",
+        "publisher": "SNK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 8,
+        "release_date": "1998-12-25",
+        "title": "サムライスピリッツ！",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 4500
+    },
+    {
+        "id": 9,
+        "release_date": "1998-12-25",
+        "title": "ネオ・チェリーマスター",
+        "publisher": "ダイナ",
+        "bit_memory": "8M",
+        "price": 4800
+    },
+    {
+        "id": 10,
+        "release_date": "1999-03-19",
+        "title": "キング・オブ・ファイターズR-2",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 4500
+    },
+    {
+        "id": 11,
+        "release_date": "1999-03-19",
+        "title": "ネオ・チェリーマスター カラー",
+        "publisher": "ダイナ",
+        "bit_memory": "8M",
+        "price": 4800
+    },
+    {
+        "id": 12,
+        "release_date": "1999-03-19",
+        "title": "ネオ・ドラゴンズワイルド",
+        "publisher": "ダイナ",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 13,
+        "release_date": "1999-03-19",
+        "title": "ネオ・ミステリーボーナス",
+        "publisher": "ダイナ",
+        "bit_memory": "8M",
+        "price": 4800
+    },
+    {
+        "id": 14,
+        "release_date": "1999-03-19",
+        "title": "ベースボールスターズ カラー",
+        "publisher": "SNK",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 15,
+        "release_date": "1999-03-19",
+        "title": "ポケットテニス カラー",
+        "publisher": "夢工房",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 16,
+        "release_date": "1999-03-19",
+        "title": "将棋の達人 カラー",
+        "publisher": "ADK",
+        "bit_memory": "4M",
+        "price": 3200
+    },
+    {
+        "id": 17,
+        "release_date": "1999-03-19",
+        "title": "連結パズル つなげてポンッ！ カラー",
+        "publisher": "夢工房",
+        "bit_memory": "4M",
+        "price": 2980
+    },
+    {
+        "id": 18,
+        "release_date": "1999-03-26",
+        "title": "パズルボブル ミニ",
+        "publisher": "タイトー／SNK",
+        "bit_memory": "8M",
+        "price": 2980
+    },
+    {
+        "id": 19,
+        "release_date": "1999-04-15",
+        "title": "クラッシュローラー",
+        "publisher": "ADK",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 20,
+        "release_date": "1999-04-15",
+        "title": "ネオジオカップ'98 プラス",
+        "publisher": "SNK",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 21,
+        "release_date": "1999-04-15",
+        "title": "バイオモーター ユニトロン",
+        "publisher": "夢工房",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 22,
+        "release_date": "1999-04-22",
+        "title": "ネオ・ダービーチャンプ 大予想",
+        "publisher": "ダイナ",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 23,
+        "release_date": "1999-04-29",
+        "title": "どこでも麻雀",
+        "publisher": "ADK",
+        "bit_memory": "4M",
+        "price": 3200
+    },
+    {
+        "id": 24,
+        "release_date": "1999-05-27",
+        "title": "ネオポケ プロ野球",
+        "publisher": "SNK／ADK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 25,
+        "release_date": "1999-05-27",
+        "title": "メタルスラッグ ファーストミッション",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 26,
+        "release_date": "1999-05-27",
+        "title": "餓狼伝説 ファーストコンタクト",
+        "publisher": "SNK／夢工房",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 27,
+        "release_date": "1999-06-10",
+        "title": "サムライスピリッツ！２",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 28,
+        "release_date": "1999-06-24",
+        "title": "マジカルドロップ ポケット",
+        "publisher": "データイースト",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 29,
+        "release_date": "1999-07-22",
+        "title": "ぷよぷよ通",
+        "publisher": "セガ／SNK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 30,
+        "release_date": "1999-07-29",
+        "title": "ビッグトーナメントゴルフ",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 31,
+        "release_date": "1999-07-29",
+        "title": "上海ミニ",
+        "publisher": "アクティヴィジョン／SNK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 32,
+        "release_date": "1999-08-19",
+        "title": "ダイヴアラート バーン編",
+        "publisher": "サクノス",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 33,
+        "release_date": "1999-08-19",
+        "title": "ダイヴアラート レベッカ編",
+        "publisher": "サクノス",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 34,
+        "release_date": "1999-08-26",
+        "title": "パックマン",
+        "publisher": "ナムコ／SNK",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 35,
+        "release_date": "1999-10-21",
+        "title": "SNK VS. CAPCOM 激突カードファイターズ CAPCOMサポーターズバージョン",
+        "publisher": "カプコン／SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 36,
+        "release_date": "1999-10-21",
+        "title": "SNK VS. CAPCOM 激突カードファイターズ SNKサポーターズバージョン",
+        "publisher": "カプコン／SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 37,
+        "release_date": "1999-10-21",
+        "title": "パチスロ アルゼ王国ポケット ハナビ",
+        "publisher": "アルゼ",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 38,
+        "release_date": "1999-10-21",
+        "title": "ビーストバスター ～闇の生体兵器～",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 39,
+        "release_date": "1999-10-21",
+        "title": "ポケットラブ ～ｉｆ～",
+        "publisher": "キッド",
+        "bit_memory": "16M",
+        "price": 4800
+    },
+    {
+        "id": 40,
+        "release_date": "1999-10-21",
+        "title": "電車でGO！２ ON ネオジオポケット",
+        "publisher": "タイトー／SNK",
+        "bit_memory": "32M",
+        "price": 4500
+    },
+    {
+        "id": 41,
+        "release_date": "1999-11-11",
+        "title": "つなげてポンッ！２",
+        "publisher": "夢工房",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 42,
+        "release_date": "1999-11-25",
+        "title": "パチンコ必勝ガイド ポケットパーラー",
+        "publisher": "ジャパンヴィステック",
+        "bit_memory": "8M",
+        "price": 3980
+    },
+    {
+        "id": 43,
+        "release_date": "1999-12-16",
+        "title": "それいけ！！ 花札道場",
+        "publisher": "ダイナ",
+        "bit_memory": "4M",
+        "price": 4800
+    },
+    {
+        "id": 44,
+        "release_date": "1999-12-22",
+        "title": "頂上決戦 最強ファイターズ SNK VS. CAPCOM",
+        "publisher": "カプコン／SNK",
+        "bit_memory": "32M",
+        "price": 2800
+    },
+    {
+        "id": 45,
+        "release_date": "1999-12-22",
+        "title": "パーティメール",
+        "publisher": "ADK",
+        "bit_memory": "8M",
+        "price": 3200
+    },
+    {
+        "id": 46,
+        "release_date": "1999-12-22",
+        "title": "ファーゼライ！",
+        "publisher": "サクノス",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 47,
+        "release_date": "1999-12-29",
+        "title": "ネオ・トゥエンティワン",
+        "publisher": "ダイナ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 48,
+        "release_date": "1999-12-29",
+        "title": "水木しげるの妖怪写真館",
+        "publisher": "水木プロ／SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 49,
+        "release_date": "2000-01-20",
+        "title": "SNK GAL'S FIGHTERS",
+        "publisher": "SNK／夢工房",
+        "bit_memory": "16M",
+        "price": 2500
+    },
+    {
+        "id": 50,
+        "release_date": "2000-01-20",
+        "title": "めざせ！ 漢字王",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3980
+    },
+    {
+        "id": 51,
+        "release_date": "2000-01-20",
+        "title": "機甲世紀ユニトロン",
+        "publisher": "夢工房",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 52,
+        "release_date": "2000-01-27",
+        "title": "ポケットリバーシ",
+        "publisher": "サクセス",
+        "bit_memory": "4M",
+        "price": 2980
+    },
+    {
+        "id": 53,
+        "release_date": "2000-02-10",
+        "title": "神機世界エヴァリューション はてしないダンジョン",
+        "publisher": "スティング／ESP",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 54,
+        "release_date": "2000-02-10",
+        "title": "パチスロ アルゼ王国ポケット アステカ",
+        "publisher": "アルゼ",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 55,
+        "release_date": "2000-02-24",
+        "title": "クルーボーダーズ ポケット",
+        "publisher": "ウエップシステム",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 56,
+        "release_date": "2000-03-09",
+        "title": "コイコイ麻雀",
+        "publisher": "ビスコ",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 57,
+        "release_date": "2000-03-09",
+        "title": "メタルスラッグ セカンドミッション",
+        "publisher": "SNK",
+        "bit_memory": "32M",
+        "price": 2500
+    },
+    {
+        "id": 58,
+        "release_date": "2000-03-16",
+        "title": "パチスロ アルゼ王国ポケット ワードオブライツ",
+        "publisher": "アルゼ",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 59,
+        "release_date": "2000-03-16",
+        "title": "ビックリマン2000 ビバ！ポケットフェスチバァ",
+        "publisher": "セガトイズ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 60,
+        "release_date": "2000-03-16",
+        "title": "幕末浪漫 特別編 月華の剣士 月に咲く花、散りゆく華",
+        "publisher": "SNK／夢工房",
+        "bit_memory": "16M",
+        "price": 2500
+    },
+    {
+        "id": 61,
+        "release_date": "2000-03-23",
+        "title": "コットン",
+        "publisher": "サクセス",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 62,
+        "release_date": "2000-04-27",
+        "title": "おえかきパズル",
+        "publisher": "サクセス",
+        "bit_memory": "4M",
+        "price": 2980
+    },
+    {
+        "id": 63,
+        "release_date": "2000-04-27",
+        "title": "メモリーズオフ・ピュア",
+        "publisher": "キッド 8M",
+        "bit_memory": "4800"
+    },
+    {
+        "id": 64,
+        "release_date": "2000-05-25",
+        "title": "ソニック・ザ・ヘッジホッグ ポケットアドベンチャー",
+        "publisher": "セガ／SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 65,
+        "release_date": "2000-05-25",
+        "title": "ダイナマイトスラッガー",
+        "publisher": "ADK",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 66,
+        "release_date": "2000-06-06",
+        "title": "がんばれねおぽけくん（仮）",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 67,
+        "release_date": "2000-06-22",
+        "title": "伝説のオウガバトル外伝 ゼノビアの皇子",
+        "publisher": "クエスト／SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 68,
+        "release_date": "2000-06-22",
+        "title": "ネオ・バカラ",
+        "publisher": "ダイナ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 69,
+        "release_date": "2000-07-06",
+        "title": "THE KING OF FIGHTERS バトルDEパラダイス",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 70,
+        "release_date": "2000-07-06",
+        "title": "パチスロ・アルゼ王国ポケット ポルカノ２",
+        "publisher": "アルゼ</TD",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 71,
+        "release_date": "2000-07-06",
+        "title": "ロックマン バトル＆ファイターズ",
+        "publisher": "カプコン",
+        "bit_memory": "16M",
+        "price": 4800
+    },
+    {
+        "id": 72,
+        "release_date": "2000-08-10",
+        "title": "クルクルジャム",
+        "publisher": "SNK",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 73,
+        "release_date": "2000-08-10",
+        "title": "デルタワープ",
+        "publisher": "イオシス",
+        "bit_memory": "4M",
+        "price": 3800
+    },
+    {
+        "id": 74,
+        "release_date": "2000-10-26",
+        "title": "パチスロ アルゼ王国ポケット デルソル２",
+        "publisher": "アルゼ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 75,
+        "release_date": "2000-11-23",
+        "title": "インフィニティ・キュア",
+        "publisher": "キッド",
+        "bit_memory": "8M",
+        "price": 4800
+    },
+    {
+        "id": 76,
+        "release_date": "2000-11-23",
+        "title": "にげロンパ",
+        "publisher": "電脳映像製作所",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 77,
+        "release_date": "2000-11-23",
+        "title": "ビッグバンプロレス",
+        "publisher": "SNK／夢工房",
+        "bit_memory": "16M",
+        "price": 3800
+    },
+    {
+        "id": 78,
+        "release_date": "2000-12-14",
+        "title": "パチスロ アルゼ王国ポケット 大花火",
+        "publisher": "アルゼ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 79,
+        "release_date": "2001-01-25",
+        "title": "パチスロ アルゼ王国ポケット デルソル2",
+        "publisher": "アルゼ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 80,
+        "release_date": "2001-03-29",
+        "title": "スーパーリアル麻雀プレミアムコレクション",
+        "publisher": "セタ／SNK",
+        "bit_memory": "16M",
+        "price": 4250
+    },
+    {
+        "id": 81,
+        "release_date": "2001-03-29",
+        "title": "パチスロ アルゼ王国ポケット イーカップ",
+        "publisher": "アルゼ",
+        "bit_memory": "8M",
+        "price": 3800
+    },
+    {
+        "id": 82,
+        "release_date": "2001-09-13",
+        "title": "SNK VS. CAPCOM カードファイターズ2 EXPAND EDITION",
+        "publisher": "カプコン／SNK",
+        "bit_memory": "16M",
+        "price": 4500
+    }
+];
